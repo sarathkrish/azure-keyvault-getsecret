c77a3c2922dc54ac147779f5b9e0851a6f823324
sarath.krishnan@slalom.com-at-617780835636 <sarath.krishnan@S01423-MBPR.local>
2020-05-12 10:37:42 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const core = require('@actions/core')
+const { ClientSecretCredential } = require("@azure/identity");
+const { SecretClient } = require("@azure/keyvault-secrets");
+
+
+
+async function main() {
+    try {
+        // Global variables
+        let Client_Id = core.getInput('Client_Id');
+        let Secret_Id = core.getInput('Secret_Id');
+        let Tenant_Id = core.getInput('Tenant_Id');
+        let secrets = core.getInput('Secrets');
+        let VaultUrl = core.getInput('VaultUrl');
+        
+        let secretsArray = secrets.split(",");
+
+        for(let i=0; i < secretsArray.length; i ++){
+
+         let value =  getSecretFromAzureKeyVault(Tenant_Id, Client_Id, Secret_Id, VaultUrl, secretsArray[i]);
+         core.setOutput(secretsArray[i], value);
+        }
+      
+
+    } catch (error) {
+       console.log("Failed to fetch Azure KeyVault Secrets!");
+        core.setFailed(error.message);
+    }
+}
+
+async function getSecretFromAzureKeyVault(Tenant_Id, Client_Id, Secret_Id, VaultUrl, secretName) {
+    try {
+        const credential = new ClientSecretCredential(Tenant_Id, Client_Id, Secret_Id);
+        const client = new SecretClient(VaultUrl, credential);
+        const latestSecret = await client.getSecret(secretName);
+        return latestSecret.value;
+    }
+    catch (err) {
+        console.log("Error in getSecretFromAzureKeyVault:" + err.message);
+        throw new Error(`Error in getSecretFromAzureKeyVault${err.message}`);
+    }
+}
+
+
+main();
